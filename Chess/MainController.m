985a2fca2abd7f50382ed79faf5e6099ad9f16c8
Japp-Galang <48370732+Japp-Galang@users.noreply.github.com>
2023-02-25 22:52:55 -0500

    Changes

--- Chess/MainController.m
+++ Chess/MainController.m
@@ -7,16 +7,17 @@
 
 #import "MainController.h"
 
+
 @interface MainController ()
 
-
-
+@property (strong, nonatomic) NSDictionary* fileToComputerFile;
 @property (nonatomic) CGFloat squareLength;
 
 @end
 
 
 @implementation MainController
+
 
 - (void)viewDidLoad
 {
@@ -54,29 +55,30 @@
     
     
 
-  
+    self.fileToComputerFile = @{@"a": @0, @"b": @1, @"c": @2, @"d": @3, @"e": @4, @"f": @5, @"g": @6, @"h": @7};
     self.pieceGrid = [NSMutableArray array];
-    [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @-1, @0, @0, @0, nil]]; // First Row
+                                                            //  a   b   c   d   e    f   g   h
+    [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @-1, @0, @0, @0, nil]];// 8
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
     [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @0, @0, @0, @0, nil]];
-    [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @1, @0, @0, @0, nil]]; // 8th Row
+    [self.pieceGrid addObject:[NSMutableArray arrayWithObjects:@0, @0, @0, @0, @1, @0, @0, @0, nil]]; // 1
     
     [self checkGridToDisplayPieces];
 }
 
 - (void)checkGridToDisplayPieces
 {
-    for(int row = 0; row < 8; row++){
-        for(int rank = 0; rank < 8; rank++){
-            if([self.pieceGrid[row][rank]  isEqual: @1]){
+    for(int rank = 0; rank < 8; rank++){
+        for(int file = 0; file < 8; file++){
+            if([self.pieceGrid[rank][file]  isEqual: @1]){
                 [self addChessIconToSquare:@"WhiteKing" file:@"e" rank:@"1" pieceIdentifier:1];
                 
             }
-            if([self.pieceGrid[row][rank]  isEqual: @-1]){
+            if([self.pieceGrid[rank][file]  isEqual: @-1]){
                 [self addChessIconToSquare:@"BlackKing" file:@"e" rank:@"8" pieceIdentifier:1];
             }
         }
@@ -112,10 +114,12 @@
 - (void)pieceTapped:(UIButton *)sender
 {
     NSString *title = [sender titleForState:UIControlStateNormal];
-    NSLog(@"Button title: %@ \nPieceIDentifier: %ld", title, (long)sender.tag);
+    //NSLog(@"Button title: %@ \nPieceIDentifier: %ld", title, (long)sender.tag);
     
     // Show available squares based on piece type and position of the board
-    
+    if(sender.tag == 1) {
+        [self kingMoves:[title characterAtIndex:0] rank:[title substringWithRange:NSMakeRange(1, 1)]];
+    }
     
     // Make those squares available to click
     
@@ -150,6 +154,44 @@
         rank = rank - 1;
     }
     NSLog(@"%@", self.displayCoordinateY);
+}
+
+
+#pragma - Each method displays how each piece can move given a position
+- (void)kingMoves:(char)file rank:(NSString*)rank
+{
+    
+    
+    // checks if there are positions available on its rank
+    if(file == 'a') {
+        
+        
+    } else if (file == 'h') {
+        
+        
+    } else {
+        if([self checkIfSquareIsAvailable:file - 1 rank:rank]) {
+            UIButton *availableMove1 = [UIButton buttonWithType:UIButtonTypeSystem];
+            
+        }
+        
+        
+    }
+    
+}
+
+
+- (BOOL)checkIfSquareIsAvailable:(char)file rank:(NSString*)rank
+{
+    NSInteger computerRank = [rank intValue] - 1;
+    NSInteger computerFile = [self.fileToComputerFile[[NSString stringWithFormat:@"%c", file]] intValue];
+    
+    if(self.pieceGrid[computerRank][computerFile] == 0){
+        return YES;
+    } else {
+        return NO;
+    }
+    
 }
 
 
